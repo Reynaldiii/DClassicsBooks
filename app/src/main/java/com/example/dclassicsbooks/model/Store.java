19cddf32713152130d7353d6007ba530aa6911ca
Reynaldiii <reynaldi191818@gmail.com>
2026-06-20 21:42:56 +0700

    adjust size

--- app/src/main/java/com/example/dclassicsbooks/model/Store.java
+++ app/src/main/java/com/example/dclassicsbooks/model/Store.java
@@ -5,10 +5,16 @@ public class Store {
     public final int imageRes;
     public final String name;
     public final String address;
+    public final String status;
+    public final String weekdayHours;
+    public final String weekendHours;
 
-    public Store(int imageRes, String name, String address) {
+    public Store(int imageRes, String name, String address, String status, String weekdayHours, String weekendHours) {
         this.imageRes = imageRes;
         this.name = name;
         this.address = address;
+        this.status = status;
+        this.weekdayHours = weekdayHours;
+        this.weekendHours = weekendHours;
     }
 }
